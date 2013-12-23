7190cedda6dbbca51ad0ad5b6879903f4131f5d9
Sam Bosley <sboz88@gmail.com>
2013-12-22 17:05:34 -1000

    Forgot to add a file in the last commit

---
+++ JavaTraitsTest/src/com/sambosley/javatraits/test/AnotherTrait.java
@@ -0,0 +1,24 @@
+package com.sambosley.javatraits.test;
+
+import com.sambosley.javatraits.annotations.Trait;
+
+@Trait
+public abstract class AnotherTrait<A, B> {
+    
+    public abstract A[] copyANTimes(A a, int n);
+    
+    public abstract B[] copyBNTimes(B b, int n);
+    
+    public int countAs(A... args) {
+        if (args != null)
+            return args.length;
+        return 0;
+    }
+    
+    public int countBs(B... args) {
+        if (args != null)
+            return args.length;
+        return 0;
+    }
+    
+}
