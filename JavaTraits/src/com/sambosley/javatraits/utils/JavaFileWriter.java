987358fb583c834c7cfc30c3e6f776edecff9d08
Sam Bosley <sboz88@gmail.com>
2014-01-12 09:31:42 -0800

    Added functions to support declaring types to JavaFileWriter

--- JavaTraits/src/com/sambosley/javatraits/utils/JavaFileWriter.java
+++ JavaTraits/src/com/sambosley/javatraits/utils/JavaFileWriter.java
@@ -16,6 +16,17 @@ public class JavaFileWriter {
     
     private Writer out;
     private Map<String, List<FullyQualifiedName>> knownNames;
+    private Scope currentScope = Scope.IMPORTS;
+    
+    private static enum Scope {
+        PACKAGE,
+        IMPORTS,
+        TYPE_DECLARATION,
+        TYPE_DEFINITION,
+        FIELD_DECLARATION,
+        METHOD_DECLARATION,
+        METHOD_DEFINITION
+    }
     
     public JavaFileWriter(Writer out) {
         if (out == null)
@@ -29,10 +40,12 @@ public class JavaFileWriter {
     }
     
     public void writePackage(String packageName) throws IOException {
+        checkScope(Scope.PACKAGE);
         out.append("package ").append(packageName).append(";\n\n");
     }
     
     public void writeImports(Collection<FullyQualifiedName> imports) throws IOException {
+        checkScope(Scope.IMPORTS);
         for (FullyQualifiedName item : imports) {
             String simpleName = item.getSimpleName();
             List<FullyQualifiedName> allNames = knownNames.get(simpleName);
@@ -49,18 +62,97 @@ public class JavaFileWriter {
         out.append("\n");
     }
     
-    public void beginType(String name, String kind, List<Modifier> modifiers) throws IOException {
+    public void beginTypeDeclaration(String name, String kind, List<Modifier> modifiers) throws IOException {
+        checkScope(Scope.IMPORTS);
         if (modifiers != null) {
             for (Modifier mod : modifiers) {
                 out.append(mod.toString()).append(" ");
             }
         }
         out.append(kind).append(" ").append(name);
+        moveToScope(Scope.TYPE_DECLARATION);
+    }
+    
+    public void appendGenericDeclaration(List<String> generics, List<FullyQualifiedName> bounds) throws IOException {
+        checkScope(Scope.TYPE_DECLARATION);
+        if (generics == null || generics.size() == 0)
+            return;
+        out.append("<");
+        if (generics.size() > 0 && bounds != null && generics.size() != bounds.size())
+            throw new IllegalArgumentException("Generics and bounds must have the same size");
         
-        // Append generics
-        // Append extends
-        // Append implements
+        for (int i = 0; i < generics.size(); i++) {
+            String generic = generics.get(i);
+            FullyQualifiedName bound = bounds != null ? bounds.get(i) : null;
+            
+            out.append(generic);
+            if (bound != null)
+                out.append(" extends ").append(shortenName(bound));
+            if (i < generics.size() - 1)
+                out.append(", ");
+        }
+        out.append(">");
+    }
+    
+    public void addSuperclassToTypeDeclaration(FullyQualifiedName superclass, List<String> generics) throws IOException {
+        checkScope(Scope.TYPE_DECLARATION);
+        out.append(" extends ").append(shortenName(superclass));
+        if (generics != null && generics.size() > 0) {
+            out.append("<");
+            for (int i = 0; i < generics.size(); i++) {
+                out.append(generics.get(i));
+                if (i < generics.size() - 1)
+                    out.append(", ");
+            }
+            out.append(">");
+        }
+    }
+    
+    public void addInterfacesToTypeDeclaration(List<FullyQualifiedName> interfaces, List<List<String>> generics) throws IOException {
+        checkScope(Scope.TYPE_DECLARATION);
+        if (interfaces != null && generics != null && interfaces.size() != generics.size())
+            throw new IllegalArgumentException("When specifying generics for implementing interfaces, lists must be the same size");
+        out.append(" implements ");
+        for (int i = 0; i < interfaces.size(); i++) {
+            out.append(shortenName(interfaces.get(i)));
+            List<String> genericsForInterface = generics.get(i);
+            if (genericsForInterface != null && generics.size() > 0) {
+                out.append("<");
+                for (int j = 0; j < genericsForInterface.size(); j++) {
+                    out.append(genericsForInterface.get(j));
+                    if (j < genericsForInterface.size() - 1)
+                        out.append(", ");
+                }
+                out.append(">");
+            }
+            if (i < interfaces.size() - 1)
+                out.append(", ");
+        }
+    }
+    
+    public void finishTypeDeclaration() throws IOException {
+        checkScope(Scope.TYPE_DECLARATION);        
+        out.append(" {\n");
+        moveToScope(Scope.TYPE_DEFINITION);
+    }
+    
+    private String shortenName(FullyQualifiedName name) {
+        String simpleName = name.getSimpleName();
+        List<FullyQualifiedName> allNames = knownNames.get(simpleName);
+        if (allNames == null || allNames.size() == 0)
+            return name.toString();
+        if (allNames.get(0).equals(name))
+            return simpleName;
+        return name.toString();
         
-        out.append("{\n");
+    }
+    
+    private void checkScope(Scope expectedScope) {
+        if (currentScope != expectedScope)
+            throw new IllegalStateException("Expected scope " + expectedScope + ", current scope " + currentScope);
+    }
+    
+    private void moveToScope(Scope moveTo) {
+        this.currentScope = moveTo;
     }
 }
