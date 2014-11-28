209e68fc7ff8bdd5f5f523358cc283573dfa7c32
Sam Bosley <sboz88@gmail.com>
2014-11-28 09:19:23 -0800

    Fix a bug in the handling of packages for primitive types. Should be empty string, not null

--- AnnotationProcessingUtils/src/com/yahoo/annotations/model/CoreTypes.java
+++ AnnotationProcessingUtils/src/com/yahoo/annotations/model/CoreTypes.java
@@ -15,14 +15,14 @@ public class CoreTypes {
     public static final DeclaredTypeName JAVA_BOOLEAN = new DeclaredTypeName(JAVA_LANG, "Boolean");
     public static final DeclaredTypeName JAVA_CHARACTER = new DeclaredTypeName(JAVA_LANG, "Character");
     
-    public static final DeclaredTypeName PRIMITIVE_INT = new DeclaredTypeName(null, "int");
-    public static final DeclaredTypeName PRIMITIVE_LONG = new DeclaredTypeName(null, "long");
-    public static final DeclaredTypeName PRIMITIVE_DOUBLE = new DeclaredTypeName(null, "double");
-    public static final DeclaredTypeName PRIMITIVE_FLOAT = new DeclaredTypeName(null, "float");
-    public static final DeclaredTypeName PRIMITIVE_SHORT = new DeclaredTypeName(null, "short");
-    public static final DeclaredTypeName PRIMITIVE_BYTE = new DeclaredTypeName(null, "byte");
-    public static final DeclaredTypeName PRIMITIVE_BOOLEAN = new DeclaredTypeName(null, "boolean");
-    public static final DeclaredTypeName PRIMITIVE_CHAR = new DeclaredTypeName(null, "char");
+    public static final DeclaredTypeName PRIMITIVE_INT = new DeclaredTypeName("", "int");
+    public static final DeclaredTypeName PRIMITIVE_LONG = new DeclaredTypeName("", "long");
+    public static final DeclaredTypeName PRIMITIVE_DOUBLE = new DeclaredTypeName("", "double");
+    public static final DeclaredTypeName PRIMITIVE_FLOAT = new DeclaredTypeName("", "float");
+    public static final DeclaredTypeName PRIMITIVE_SHORT = new DeclaredTypeName("", "short");
+    public static final DeclaredTypeName PRIMITIVE_BYTE = new DeclaredTypeName("", "byte");
+    public static final DeclaredTypeName PRIMITIVE_BOOLEAN = new DeclaredTypeName("", "boolean");
+    public static final DeclaredTypeName PRIMITIVE_CHAR = new DeclaredTypeName("", "char");
     
     public static final DeclaredTypeName OVERRIDE = new DeclaredTypeName(JAVA_LANG, "Override");
     public static final DeclaredTypeName DEPRECATED = new DeclaredTypeName(JAVA_LANG, "Deprecated");
