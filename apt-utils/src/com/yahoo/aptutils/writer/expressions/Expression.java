3a60e3c282472f857001cbc7e4175b5a8c24fe5f
Sam Bosley <sboz88@gmail.com>
2015-01-14 09:40:06 -0800

    Make sure the copyright notice is in every file

--- apt-utils/src/com/yahoo/aptutils/writer/expressions/Expression.java
+++ apt-utils/src/com/yahoo/aptutils/writer/expressions/Expression.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 Yahoo Inc.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.yahoo.aptutils.writer.expressions;
 
 import com.yahoo.aptutils.model.TypeName;
@@ -7,7 +22,7 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- Expression represent various types of expressions you might find in a typical Java file, and are useful
+ * Expression represent various types of expressions you might find in a typical Java file, and are useful
  * when writing files using {@link com.yahoo.aptutils.writer.JavaFileWriter}. For example--say you want to emit a
  * statement that calls a method on some object, where the argument to that method is a method call on another object.
  * This can be composed entirely using expressions:
